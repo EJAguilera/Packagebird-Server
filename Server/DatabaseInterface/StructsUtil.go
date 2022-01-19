f984cb82937a6907201ec0b6c25100c852b60020
EJAguilera <elisha.aguilera@wsu.edu>
2022-01-18 22:02:24 -0800

    Added untested dependency-graph building function

--- Server/DatabaseInterface/StructsUtil.go
+++ Server/DatabaseInterface/StructsUtil.go
@@ -8,6 +8,7 @@ type Package struct {
 	Version      int64
 	Source       string    // Path to source code, binaries on disk for server
 	Dependencies []Package // Recursive reference to other packages
+	Graph        []DependencyGraph
 }
 
 type Project struct {
@@ -21,4 +22,28 @@ type Project struct {
 type Team struct {
 	TeamName string
 	Members  []Member
+}
+
+type DependencyGraph struct {
+	Dependencies []Package
+}
+
+// Recursively iterates through list of dependencies, building on passed dependency-graph through parameter
+func BuildGraph(item Package, graph DependencyGraph) {
+	for _, dep := range item.Dependencies {
+
+		// Special loop that checks that package hasn't already been added to dependency-graph to prevent duplicates
+		var found bool = false
+		for _, dups := range graph.Dependencies {
+			if dep.UUID == dups.UUID {
+				found = true
+			}
+		}
+
+		// If item not already found, add to list 'graph', recursively call on package to see if more dependencies nested
+		if !found {
+			graph.Dependencies = append(graph.Dependencies, dep)
+			BuildGraph(dep, graph)
+		}
+	}
 }
